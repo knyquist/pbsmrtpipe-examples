b8784285b6468f07153e1006fbddf7c8957c7e72
Kristofor Nyquist <knyquist@pacificbiosciences.com>
2016-06-14 19:48:56 -0700

    troubleshooting tool-contract generation

--- interact_with_multiple_mapped_ssets.py
+++ interact_with_multiple_mapped_ssets.py
@@ -3,6 +3,11 @@
 import sys
 import logging
 import csv
+import random
+import pickle
+
+sys.path.append( '/mnt/usmp-data3/scratch/Labs/Kristofor/python/mhpbb' )
+import milhouseBAM as mh
 
 from pbcommand.models import FileTypes
 from pbcommand.cli import registry_builder, registry_runner
@@ -23,53 +28,93 @@ NAMESPACE = "pbsmrtpipe_examples"
 registry = registry_builder(NAMESPACE, "interact_with_multiple_mapped_ssets.py" )
 
 
+def _get_dset_paths( input_file ):
+	dset_paths = []
+	log.info( "Attempting to open input CSV" )
+	with open( input_file, 'rb' ) as csvfile:
+		reader = csv.reader( csvfile )
+		for mapped_sset in reader:
+			if mapped_sset[0][0] is not '#': # check for a commented line (like a header)
+				absolute_filename = mapped_sset[0]
+				dset_paths.append( absolute_filename )
+	return dset_paths
+
+def _subsample_alignments( mapped_subreadset, num=1000 ):
+	ss = random.sample( mapped_subreadset, num )
+	return ss
+
+def _getKPIs( mapped_sset, subsampled_mapped_sset ):
+	"""
+	Retrieve the KPIs for a single mapped sset in a dictionary structure.
+	"""
+	data                   = {}
+	data[ 'holenumber' ]   = []
+	data[ 'readlength' ]   = []
+	data[ 'templatespan' ] = []
+	data[ 'insertions' ]   = []
+	data[ 'deletions' ]    = []
+	data[ 'mismatches' ]   = []
+	data[ 'accuracy' ]     = []
+	data[ 'IPD' ]          = []
+
+	for aln in subsampled_mapped_sset:
+		data[ 'holenumber'   ].append( aln.HoleNumber )
+		data[ 'readlength'   ].append( float( aln.readEnd - aln.readStart ) )
+		data[ 'templatespan' ].append( float( aln.referenceEnd - aln.referenceStart ) )
+		data[ 'insertions'   ].append( float( aln.nIns ) / data[ 'readlength' ][-1] )
+		data[ 'deletions'    ].append( float( aln.nDel ) / data[ 'readlength' ][-1] )
+		data[ 'mismatches'   ].append( float( aln.nMM )  / data[ 'readlength' ][-1] )
+		error_rate =   ( aln.nIns + aln.nDel + aln.nMM ) / data[ 'readlength' ][-1]
+		data[ 'accuracy'     ].append( 1 - error_rate )
+		data[ 'IPD'          ].append( aln.IPD() )
+
+	data[ 'total nreads' ]  = len( mapped_sset )
+	
+	return data
+
 def _example_main( input_file, output_file, **kwargs ):
-    """
-    This func should be imported from your python package.
+	"""
+	This func should be imported from your python package.
 
-    This should have *no* dependency on the pbcommand IO, such as the RTC/TC models.
-    """
+	This should have *no* dependency on the pbcommand IO, such as the RTC/TC models.
+	"""
 
-    # This is just for test purposes
-    log.info("Running example main with {i} {o} kw:{k}".format(i=input_file,
-                                                               o=output_file,
-                                                               k=kwargs))
+	# This is just for test purposes
+	log.info("Running example main with {i} {o} kw:{k}".format(i=input_file,
+																o=output_file,
+																k=kwargs))
 
-    # Try to open input CSV. Iterate through mapped ssets
-    dsets = []
-    log.info( "Attempting to open input CSV" )
-    with open( input_file, 'rb' ) as csvfile:
-        reader = csv.reader( csvfile )
-        for mapped_sset in reader:
-            if mapped_sset[0] is not '#': # check for a commented line (like a header)
-                dsets.append( mapped_sset )
+	# Open input CSV. Store absolute path of each alignment set.
+	dset_paths = _get_dset_paths( input_file )
 
-    # write dset
-    with open( output_file, 'wb' ) as csvfile:
-        writer = csv.writer( csvfile )
-        for dset in dsets:
-            writer.writerow( dset )
+	dsets_kpis = {}
+	for f in dset_paths:
+		dset = openDataSet( f )
+		subsampled_dset = _subsample_alignments( dset )
+		dsets_kpis[ f ] = _getKPIs( dset, subsampled_dset )
 
-    return 0
+	pickle.dump( dset_kpis, open( output_file, 'wb' ) )
+
+	return 0
 
 
-@registry( "dev_interact_with_multiple_mapped_ssets", "0.2.2", ( FileTypes.CSV, ), ( FileTypes.CSV, ), nproc = 1, options = dict( alpha = 1234) )
+@registry( "dev_interact_with_multiple_mapped_ssets", "0.2.2", ( FileTypes.CSV, ), ( FileTypes.PICKLE, ), nproc = 1, options = dict( alpha = 1234) )
 def run_rtc(rtc):
-    """
-    Example Task for grabbing data from multiple mapped ssets. Single input CSV contains path to each mapped sset.
-    Takes a mapped SubreadSet XML file as input and writes a csv file with mock data.
-    """
-    # The above docstring will be used as the Task/ToolContract Description
+	"""
+	Example Task for grabbing data from multiple mapped ssets. Single input CSV contains path to each mapped sset.
+	Takes a mapped SubreadSet XML file as input and writes a csv file with mock data.
+	"""
+	# The above docstring will be used as the Task/ToolContract Description
 
-    log.info("Got RTC task options {t}".format(t=rtc.task.options))
-    log.info("Got nproc {n}".format(n=rtc.task.nproc))
+	log.info("Got RTC task options {t}".format(t=rtc.task.options))
+	log.info("Got nproc {n}".format(n=rtc.task.nproc))
 
-    # The Task options are now accessible via global identifier
-    alpha = rtc.task.options['pbsmrtpipe_examples.task_options.alpha']
-    return _example_main( rtc.task.input_files[0], rtc.task.output_files[0], nproc = rtc.task.nproc, alpha = alpha )
+	# The Task options are now accessible via global identifier
+	alpha = rtc.task.options['pbsmrtpipe_examples.task_options.alpha']
+	return _example_main( rtc.task.input_files[0], rtc.task.output_files[0], nproc = rtc.task.nproc, alpha = alpha )
 
 
-if __name__ == '__main__':
-    sys.exit(registry_runner(registry, sys.argv[1:]))
+	if __name__ == '__main__':
+		sys.exit(registry_runner(registry, sys.argv[1:]))
 
 
